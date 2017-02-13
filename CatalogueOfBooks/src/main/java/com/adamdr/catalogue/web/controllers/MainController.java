70723f1bcb30fbabed5e906e715df1f45a78bbee
adamdrzewiecki <drzevoo@gmail.com>
2017-02-13 19:25:18 +0100

    New project first commit

---
+++ CatalogueOfBooks/src/main/java/com/adamdr/catalogue/web/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.adamdr.catalogue.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping({"/home", "/"})
+    public String homeForm() {
+        return "home";
+    }
+}
