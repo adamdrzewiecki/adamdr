7f665b464800655cb61de410d2e0df1cf4accc7f
adamdrzewiecki <drzevoo@gmail.com>
2017-02-22 19:22:05 +0100

    Login patch

--- CatalogueOfBooks/src/main/java/com/adamdr/catalogue/config/SpringSecurityConfig.java
+++ CatalogueOfBooks/src/main/java/com/adamdr/catalogue/config/SpringSecurityConfig.java
@@ -3,12 +3,14 @@ package com.adamdr.catalogue.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -20,15 +22,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/login", "/signup").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
+        http.formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/home")
+                .defaultSuccessUrl("/home", true)
+                .failureUrl("/login?error=true")
                 .and()
-                .logout().logoutSuccessUrl("/signout");
+            .logout()
+                .logoutSuccessUrl("/login");
     }
 }
--- CatalogueOfBooks/src/main/java/com/adamdr/catalogue/web/controllers/MainController.java
+++ CatalogueOfBooks/src/main/java/com/adamdr/catalogue/web/controllers/MainController.java
@@ -2,14 +2,10 @@ package com.adamdr.catalogue.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
-
-    @GetMapping({"/login"})
-    public String loginForm() {
-        return "login";
-    }
 
     @GetMapping({"/home"})
     public String home() {
